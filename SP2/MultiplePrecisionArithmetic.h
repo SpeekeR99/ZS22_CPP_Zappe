5f39ada48950e7d4d6190215e08e5e011a397bd1
SpeekeR <DominikZappe1999@seznam.cz>
2022-12-02 20:57:28 +0100

    + and += operators working

--- SP2/MultiplePrecisionArithmetic.h
+++ SP2/MultiplePrecisionArithmetic.h
@@ -10,23 +10,13 @@ constexpr uint8_t MAX_DIGITS = 9;
 constexpr int8_t UNLIMITED = -1;
 
 template <typename T>
-concept LimitedDigit = requires(T t) {
-    t >= 4;
-};
-
-template <typename T>
-concept UnlimitedDigit = requires(T t) {
-    t == UNLIMITED;
-};
-
-template <typename T>
 concept ValidDigit = requires (T t) {
     std::is_integral_v<T>;
-    LimitedDigit<T> || UnlimitedDigit<T>;
+    t >= MAX_DIGITS || t == UNLIMITED; // 4 byte number ~ 9 digit number (thus MAX_DIGITS)
 };
 
-template <typename T, T x, T y>
-requires ValidDigit<T>
+template <int32_t x, int32_t y>
+requires ValidDigit<int32_t>
 struct MaxDigits {
     static const int32_t value = (x == UNLIMITED || y == UNLIMITED) ? UNLIMITED : (x > y) ? x : y;
 };
@@ -34,92 +24,104 @@ struct MaxDigits {
 template <int32_t max_digits>
 class MPInt {
 private:
-    std::vector<uint32_t> number;
-    int8_t sign;
+    std::vector<uint32_t> mNumber;
+    int8_t mSign;
 
     void checkLengthOfDigits() const {
         if (max_digits == UNLIMITED)
             return;
 
-        if (number.size() * MAX_DIGITS > max_digits) {
-            std::string str_num = std::to_string(number[number.size() - 1]);
+        if (mNumber.size() * MAX_DIGITS > max_digits) {
+            std::string str_num = std::to_string(mNumber[mNumber.size() - 1]);
             if (str_num.length() > max_digits % MAX_DIGITS)
                 throw std::overflow_error("Number " + toString() + " is too big for " + std::to_string(max_digits) + " digits");
         }
     }
 
+    std::vector<uint32_t> add(const std::vector<uint32_t> &num1, const std::vector<uint32_t> &num2) {
+        std::vector<uint32_t> result_number;
+
+        uint8_t carry = 0;
+        for (size_t i = 0; i < num1.size() || i < num2.size() || carry; i++) {
+            uint32_t sum = (num1.size() > i ? num1[i] : 0) +
+                           (num2.size() > i ? num2[i] : 0) + carry;
+            carry = sum / BASE;
+            sum %= BASE;
+            result_number.push_back(sum);
+        }
+
+        return result_number;
+    }
+
 public:
-    explicit MPInt(const std::string &num) : number(), sign(1) {
+    explicit MPInt(const std::string &num) : mNumber(), mSign(1) {
         std::string num_copy = num;
 
         if (num[0] == '-') {
-            sign = -1;
+            mSign = -1;
             num_copy = num.substr(1);
         }
 
         for (auto i = static_cast<int32_t>(num_copy.length()); i > 0; i -= MAX_DIGITS) {
             if (i < MAX_DIGITS)
-                number.push_back(std::stoi(num_copy.substr(0, i)));
+                mNumber.push_back(std::stoi(num_copy.substr(0, i)));
             else
-                number.push_back(std::stoi(num_copy.substr(i - MAX_DIGITS, MAX_DIGITS)));
+                mNumber.push_back(std::stoi(num_copy.substr(i - MAX_DIGITS, MAX_DIGITS)));
         }
 
         checkLengthOfDigits();
     }
 
-    MPInt(std::vector<uint32_t> num, int8_t sign) : number(std::move(num)), sign(sign) {
+    MPInt(std::vector<uint32_t> num, int8_t sign) : mNumber(std::move(num)), mSign(sign) {
         checkLengthOfDigits();
     }
+
+    // TODO - copy constructor, copy assignment, move constructor, move assignment
 
     ~MPInt() = default;
 
     template<int32_t max_digits_other>
-    MPInt<MaxDigits<decltype(max_digits), max_digits, max_digits_other>::value> operator+(const MPInt<max_digits_other> &other) {
+    MPInt<MaxDigits<max_digits, max_digits_other>::value> operator+(const MPInt<max_digits_other> &other) {
         // Adding two positive or two negative numbers
-        if (sign == other.getSign()) {
-            if (sign == 1)
-                return {add(*this, other), 1};
-            else
-                return {add(*this, other), -1};
-        }
+        if (mSign == other.getSign())
+            return {add(mNumber, other.getNumber()), mSign};
         // Addition -> Subtraction
         else {
             // TODO
+            return {std::vector<uint32_t>(), 0};
         }
-        return {std::vector<uint32_t>{}, 1};
+    }
+
+    template<int32_t max_digits_other>
+    MPInt operator+=(const MPInt<max_digits_other> &other) {
+        auto result = *this + other;
+        if (MaxDigits<max_digits, max_digits_other>::value != max_digits)
+            throw std::overflow_error("Number " + result.toString() + " is too big for " + std::to_string(max_digits) + " digits");
+        mNumber = result.getNumber();
+        mSign = result.getSign();
+        return *this;
     }
 
     std::string toString() const {
         std::stringstream result;
-        if (sign == -1)
+        if (mSign == -1)
             result << "-";
-        for (int32_t i = number.size() - 1; i >= 0; i--)
-            result << number[i];
+        result << mNumber[mNumber.size() - 1];
+        for (int32_t i = static_cast<int32_t>(mNumber.size()) - 2; i >= 0; i--) {
+            std::string str_num = std::to_string(mNumber[i]);
+            for (int32_t j = 0; j < MAX_DIGITS - str_num.length(); j++) // "non leading" zeros
+                result << "0";
+            result << str_num;
+        }
         return result.str();
     }
 
     std::vector<uint32_t> getNumber() const {
-        return number;
+        return mNumber;
     }
 
     int8_t getSign() const {
-        return sign;
+        return mSign;
     }
 
 };
-
-template<int32_t max_digits1, int32_t max_digits2>
-std::vector<uint32_t> add(const MPInt<max_digits1> &num1, const MPInt<max_digits2> &num2) {
-    std::vector<uint32_t> result_number;
-
-    uint8_t carry = 0;
-    for (size_t i = 0; i < num1.getNumber().size() || i < num2.getNumber().size() || carry; i++) {
-        int32_t sum = (num1.getNumber().size() > i ? num1.getNumber()[i] : 0) +
-                      (num2.getNumber().size() > i ? num2.getNumber()[i] : 0) + carry;
-        carry = sum / BASE;
-        sum %= BASE;
-        result_number.push_back(sum);
-    }
-
-    return result_number;
-}
--- SP2/main.cpp
+++ SP2/main.cpp
@@ -2,12 +2,12 @@
 #include "MultiplePrecisionArithmetic.h"
 
 int main() {
-    MPInt<30> cislo1("1111100900");
-    MPInt<10> cislo2("1111111111");
+    MPInt<50> cislo1("111111111000001111");
+    MPInt<30> cislo2("111111111111111111");
     auto vysledek = cislo1 + cislo2;
 
-    MPInt<UNLIMITED> cislo3("-1234567890123456789012345678901234567890123456789023");
-    MPInt<UNLIMITED> cislo4("-55555555555555555555555555555555555555555555555555555555555555555555555");
+    MPInt<UNLIMITED> cislo3("1234567890123456789012345678901234567890123456789023");
+    MPInt<UNLIMITED> cislo4("55555555555555555555555555555555555555555555555555555555555555555555555");
     auto vysledek2 = cislo3 + cislo4;
 
     std::cout << cislo1.toString() << std::endl;
@@ -17,6 +17,24 @@ int main() {
     std::cout << cislo3.toString() << std::endl;
     std::cout << cislo4.toString() << std::endl;
     std::cout << vysledek2.toString() << std::endl;
+
+    std::cout << cislo1.toString() << std::endl;
+    try {
+        cislo1 += cislo3;
+    }
+    catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << cislo1.toString() << std::endl;
+
+    std::cout << cislo1.toString() << std::endl;
+    try {
+        cislo1 += cislo2;
+    }
+    catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << cislo1.toString() << std::endl;
 
     return 0;
 }
